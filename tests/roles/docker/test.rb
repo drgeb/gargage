da33acaf11ef70e3d9fa2ac329d49308501a91fb
Jon Burroughs <kb9zzw@gmail.com>
2020-03-28 17:53:10 -0500

    dev

---
+++ tests/roles/docker/test.rb
@@ -0,0 +1,11 @@
+describe command('docker') do
+  it {should exist}
+  its('exit_status') {should eq 0}
+end
+
+describe group('docker') do
+  it {should exist}
+  its('members') {should include 'kitchen'}
+end
+
+
