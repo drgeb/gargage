fd6d9ce888ac8fdb92c3998f7b2b2514a54cc553
Jon Burroughs <kb9zzw@gmail.com>
2020-03-28 18:37:11 -0500

    bootstrap dev

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,6 @@ PLAYBOOK_SRC=https://gitlab.com/kb9zzw/dev-bootstrap.git
 PLAYBOOK_DIR=~/.ansible/playbooks
 PLAYBOOK_DEST="${PLAYBOOK_DIR}/dev-bootstrap"
 PLAYBOOK="${1:-playbook.yml}"
-REV="${2:-master}"
 
 # Install Ansible
 if [ -e /etc/os-release ] && grep 'ubuntu' /etc/os-release > /dev/null; then
@@ -21,17 +20,12 @@ else
   exit 1
 fi
 
-# Retrieve latest playbook
+# Retrieve playbook
 if [ ! -e "${PLAYBOOK_DEST}" ]; then
   mkdir -p ${PLAYBOOK_DIR}
   git clone "${PLAYBOOK_SRC}" "${PLAYBOOK_DEST}" || exit 1
-else
-  cd "${PLAYBOOK_DEST}" || exit 1
-  git pull origin master || exit 1
 fi
 
 # Run the playbook
 cd "${PLAYBOOK_DEST}" || exit 1
-git checkout "${REV}"
-
 ansible-playbook "${PLAYBOOK}" -i hosts
