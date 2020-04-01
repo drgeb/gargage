ee2fb223d33b0ebd1b41732a570746ea9b3c2b59
Jon Burroughs <kb9zzw@gmail.com>
2020-04-01 12:05:03 -0500

    added postgresql

---
+++ tests/roles/postgresql/test.rb
@@ -0,0 +1,10 @@
+describe command('psql --version') do
+  its('stdout') { should match (/psql \(PostgreSQL\)/) }
+  its('exit_status') { should eq 0 }
+end
+
+unless os.name == 'amazonlinux'
+  describe command('pgadmin4') do
+    it { should exist }
+  end
+end
