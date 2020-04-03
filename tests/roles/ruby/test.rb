dfaae1e52d58a4d26849475ede17e7f328727264
Jon Burroughs <kb9zzw@gmail.com>
2020-04-03 16:06:28 -0500

    dev

--- tests/roles/ruby/test.rb
+++ tests/roles/ruby/test.rb
@@ -1,3 +1,5 @@
-describe file('~/.rbenv') do
+home = command('echo $HOME').stdout.strip
+
+describe file("#{home}/.rbenv") do
   it {should exist}
 end
--- tests/roles/terraform/test.rb
+++ tests/roles/terraform/test.rb
@@ -1,3 +1,5 @@
-describe command('~/.local/bin/terraform --version') do
+home = command('echo $HOME').stdout.strip
+
+describe command("#{home}/.local/bin/terraform --version") do
   its('exit_status') { should eq 0 }
 end
