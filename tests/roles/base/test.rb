7322a504144d03495eae397d960245bf9e7605ab
Jon Burroughs <kb9zzw@gmail.com>
2020-04-05 07:54:08 -0500

    updated base test

--- tests/roles/base/test.rb
+++ tests/roles/base/test.rb
@@ -18,11 +18,14 @@ nvim --version
 make -v
 gcc -v
 jq --version
-certutil -H
 git --version}.split("\n")
 
 test_commands.each do |cmd|
   describe command(cmd) do
-    its('exit_status') { should eq 0 }
+    its('exit_status') {should eq 0}
   end
+end
+
+describe command('certutil -H') do
+  its('exit_status') {should eq 1}
 end
